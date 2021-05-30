3719ded12b27821a4d59fd76772111fbcab11174
MaybeNotThomas <tomtomrainer@gmail.com>
2021-05-30 10:37:04 +0800

    finished name change

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+let ranks = JSON.parse(FileLib.getUrlContent("https://raw.githubusercontent.com/MaybeNotThomas/iebridge/main/ranks.json"))
+
 register("chat", function(event) {
   var msg = ChatLib.getChatMessage(event, true).toString();
   if(msg.removeFormatting().startsWith("Guild > ")){
   	cancel(event)
-    //msg = msg.replace("", "")
-    msg = msg.replace("&r&2Guild > &a[VIP] IceEssenceBot &6[OFF]&f: ","§fIce§bEssence §8Dis§9cord §f> §b")
+    for (let key in ranks) {
+      if (ranks.hasOwnProperty(key)) {
+        if (msg.includes(String(key))) {
+          msg = msg.replace(String(key), String(ranks[String(key)]))
+        }
+      }
+    }
+    msg = msg.replace("&r&2Guild > &a[VIP] IceEssenceBot &6[OFF]&f: &r","&fIce&bEssence &8Dis&9cord &f> &b")
     msg = msg.replace(": ", "&f: ")
-  	ChatLib.chat(new Message(msg))
+    ChatLib.chat(msg)
   }
 })
+
+register("command", () => {
+  let ranks = JSON.parse(FileLib.getUrlContent("https://raw.githubusercontent.com/MaybeNotThomas/iebridge/main/ranks.json"))
+  ChatLib.chat(JSON.stringify(ranks))
+}).setName("reload_name_list")
+
+register("command", () => {
+  ChatLib.chat(new Message("&r&2Guild > &b[MVP&9+&b] idkwhat2plus2is&f: asd").setRecursive(true))
+}).setName("test_msg")
